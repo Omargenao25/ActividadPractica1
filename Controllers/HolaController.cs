76cced856c1f396ca0f227696f3f5328a6e0ab0f
Omargenao25 <omargenao78@gmail.com>
2026-06-02 22:34:32 -0400

    creacion de ActividadPractica1 y primer controlador hola.

---
+++ Controllers/HolaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    [ApiController]
+    [Route("api/hola")]
+    public class HolaController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
