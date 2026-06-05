b6dfb532e123c37e406f221273be32e66e4e22c7
Omargenao25 <omargenao78@gmail.com>
2026-06-05 18:25:32 -0400

    creacion de FechaController

---
+++ Controllers/FechasController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    [ApiController]
+    [Route("api/fechas")]
+    public class FechasController : ControllerBase
+
+
+
+        }
+    }
+}
