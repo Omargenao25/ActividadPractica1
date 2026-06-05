f31c4585d36603c743e3931fdb7550c18ba005c4
Omargenao25 <omargenao78@gmail.com>
2026-06-05 16:55:23 -0400

    terminacion del ImcController

--- Controllers/ImcController.cs
+++ Controllers/ImcController.cs
@@ -12,11 +12,32 @@ namespace ActividadPractica1.Controllers
             double imc = peso / (altura * altura);
 
             string categoria;
-            
 
+            if (imc < 18.5)
+            {
+                categoria = "Bajo peso";
+            }
+            else if (imc < 25)
+            {
+                categoria = "Normal";
+            }
+            else if (imc < 30)
+            {
+                categoria = "Sobrepeso";
+            }
+            else
+            {
+                categoria = "Obesidad";
+            }
 
+            return Ok(new
+            {
+                peso,
+                altura,
+                imc = Math.Round(imc, 2),
+                categoria
+            });
 
-            
         }
     }
 }
