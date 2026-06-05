b89511cc3733ba020eb3bebd2398a93108ef154f
Omargenao25 <omargenao78@gmail.com>
2026-06-04 23:37:24 -0400

    creacion de numerosController.

---
+++ Controllers/NumerosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    [ApiController]
+    [Route("api/numeros")]
+    public class NumerosController : Controller
+    {
+        [HttpGet("analizar")]
+        public IActionResult Analizar([FromQuery] int n)
+        {
+            return Ok(new
+            {
+                numero = n,
+                parOimpar = n % 2 == 0 ? "Par" : "Impar",
+                esPrimo = EsPrimo(n),
+                signo = n > 0 ? "Positivo" : n < 0 ? "Negativo" : "Cero"
+            });
+        }
+
+        private static bool EsPrimo(int numero)
+        {
+            if (numero < 2)
+            {
+                return false;
+            }
+
+            for (var divisor = 2; divisor <= Math.Sqrt(numero); divisor++)
+            {
+                if (numero % divisor == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
