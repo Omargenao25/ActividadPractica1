d7e55deb884e490a5c9ced69ad93181867cab888
Omargenao25 <omargenao78@gmail.com>
2026-06-05 16:00:40 -0400

    creacion de ImcController

---
+++ Controllers/ImcController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    [ApiController]
+    [Route("api/imc")]
+    public class ImcController : ControllerBase
+    {
+        [HttpGet("calcular")]
+        public IActionResult Calcular(double peso, double altura)
+        {
+            double imc = peso / (altura * altura);
+
+            string categoria;
+            
+
+
+
+            
+        }
+    }
+}
