60cba164d2278efa4fc0179c610600e49a9ad8eb
Omargenao25 <omargenao78@gmail.com>
2026-06-05 17:18:20 -0400

    creacion de TiempoController

---
+++ Controllers/TiempoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    [ApiController]
+    [Route("api/tiempo")]
+    public class TiempoController : ControllerBase
+    {
+        [HttpGet("formatear")]
+        public IActionResult Formatear(int segundos)
+        {
+            int horas = segundos / 3600;
+
+            int minutos = (segundos % 3600) / 60;
+
+            int segundosRestantes = segundos % 60;
+
+            string formato = $"{horas:D2}: {minutos:D2}:{segundosRestantes:D2}";
