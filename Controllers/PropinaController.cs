4ea2364b9654dc4c8a45b81067388ca32c01e548
Omargenao25 <omargenao78@gmail.com>
2026-06-06 18:18:32 -0400

    cracion de propina controller

---
+++ Controllers/PropinaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    [ApiController]
+    [Route("api/propina")]
+    public class PropinaController : ControllerBase
+    {
+        [HttpGet("calcular")]
+        public IActionResult Calcular(decimal monto, decimal porcentaje)
+        {
+            decimal propina = monto * porcentaje / 100;
+
+            decimal total = monto + propina;
+        }
+}
+}
