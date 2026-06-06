4fc1da71e97b8c54c618ec8ecd6c15c5c1de7652
Omargenao25 <omargenao78@gmail.com>
2026-06-06 17:41:30 -0400

    terminacion de fecha controller

--- Controllers/FechasController.cs
+++ Controllers/FechasController.cs
@@ -5,6 +5,31 @@ namespace ActividadPractica1.Controllers
     [ApiController]
     [Route("api/fechas")]
     public class FechasController : ControllerBase
+    {
+        [HttpGet("diferencia")]
+        public IActionResult Diferencia(DateTime desde, DateTime hasta)
+        {
+            int dias = (hasta - desde).Days;
+
+            return Ok(new
+            {
+                desde,
+                hasta,
+                dias
+            });
+        }
+
+        [HttpGet("agregar")]
+        public IActionResult Agregar(DateTime fecha, int dias)
+        {
+            DateTime nuevaFecha = fecha.AddDays(dias);
+
+            return Ok(new
+            {
+                fechaOriginal = fecha,
+                diasAgregados = dias,
+                nuevaFecha
+            });
 
 
 
