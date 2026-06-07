28e538f2a5174423b74a4e2fe03e58e835134af6
Omargenao25 <omargenao78@gmail.com>
2026-06-06 20:53:30 -0400

    creacion de edad controller

---
+++ Controllers/EdadController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    [ApiController]
+    [Route("api/edad")]
+    public class EdadController : ControllerBase
+
+
+
+
+        }
+    }
+}
