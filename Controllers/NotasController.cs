d638cb9c8f3f61996012279792f85ca7904ade9d
Omargenao25 <omargenao78@gmail.com>
2026-06-10 17:08:48 -0400

    terminacion de notas controller

--- Controllers/NotasController.cs
+++ Controllers/NotasController.cs
@@ -11,6 +11,37 @@ namespace ActividadPractica1.Controllers
         {
             double promedio = datos.Notas.Average();
 
+            int aprobados = 0;
+            int reprobados = 0;
+
+            foreach (double nota in datos.Notas)
+            {
+                if (nota >= 70)
+                {
+                    aprobados++;
+                }
+                else
+                {
+                    reprobados++;
+                }
+            }
+
+            return Ok(new
+            {
+                promedio = Math.Round(promedio, 2),
+                notaMayor = datos.Notas.Max(),
+                notaMenor = datos.Notas.Min(),
+                aprobados,
+                reprobados
+            });
         }
     }
+
+
+    public class NotasRequest
+    {
+        public List<double> Notas { get; set; } = new List<double>();
+    }
 }
+
+
