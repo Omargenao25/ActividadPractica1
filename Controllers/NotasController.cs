7b1d1e4fd8055aa2bd35cadca1dd460cf4242c51
Omargenao25 <omargenao78@gmail.com>
2026-06-10 15:31:40 -0400

    creacion de notas controller

---
+++ Controllers/NotasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadPractica1.Controllers
+{
+    public class NotasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
